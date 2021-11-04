005e6c1952eb797931204d602d5e0648f1bbb946
greg.gajda@digital.hmrc.gov.uk <29655695+greg-gajda@users.noreply.github.com>
2021-11-04 18:13:12 +0000

    path module

--- main.tf
+++ main.tf
@@ -1,42 +1,39 @@
 variable "cmd" {
   description = "The command used to create the resource."
 }
-
 variable "destroy_cmd" {
   description = "The command used to destroy the resource."
 }
-
 variable "account_id" {
   description = "The account that holds the role to assume in. Will use providers account by default"
-  default     = "0"
+  default = "0"
 }
-
 variable "role" {
   description = "The role to assume in order to run the cli command."
-  default     = "0"
+  default = "0"
 }
-
 variable "dependency_ids" {
   description = "IDs or ARNs of any resources that are a dependency of the resource created by this module."
-  type        = "list"
-  default     = []
+  type = "list"
+  default = []
 }
 
 data "aws_caller_identity" "id" {}
 
 locals {
-  assume_role_cmd = "source ${path.module}/assume_role.sh ${var.account_id} ${var.role}"
+  account_id      = "${var.account_id == 0 ? data.aws_caller_identity.id.account_id : var.account_id}"
+  assume_role_cmd = "source ./assume_role.sh ${local.account_id} ${var.role}"
 }
 
 resource "null_resource" "cli_resource" {
   provisioner "local-exec" {
     when    = "create"
-    command = "/bin/bash -c '${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.cmd}'"
+    command = "${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.cmd}"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "/bin/bash -c '${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}'"
+    command = "${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}"
   }
 
   # By depending on the null_resource, the cli resource effectively depends on the existance
@@ -52,5 +49,5 @@ resource "null_resource" "dependencies" {
 
 output "id" {
   description = "The ID of the null_resource used to provison the resource via cli. Useful for creating dependencies between cli resources"
-  value       = "${null_resource.cli_resource.id}"
+  value = "${null_resource.cli_resource.id}"
 }
