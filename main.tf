a75f6a5ce49f1d2bd90bbf88eb4b6066249396b8
greg.gajda@digital.hmrc.gov.uk <29655695+greg-gajda@users.noreply.github.com>
2022-01-12 13:25:34 +0000

    All cmds in one go

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ variable "role" {
 
 variable "dependency_ids" {
   description = "IDs or ARNs of any resources that are a dependency of the resource created by this module."
-  type        = list(string)
+  type        = "list"
   default     = []
 }
 
@@ -29,46 +29,30 @@ locals {
   assume_role_cmd = "source /tmp/assume_role.sh ${local.account_id} ${var.role}"
 }
 
-resource "null_resource" "assume_role" {
-  triggers = {
-    cmd = "echo cm9sZV9zZXNzaW9uX25hbWU9YGNhdCAvcHJvYy9zeXMva2VybmVsL3JhbmRvbS91dWlkIDI+L2Rldi9udWxsIHx8IGRhdGUgfCBja3N1bSB8IGN1dCAtZCAiICIgLWYgMWAKYXdzX2NyZWRzPSQoYXdzIHN0cyBhc3N1bWUtcm9sZSAtLXJvbGUtYXJuIGFybjphd3M6aWFtOjokMTpyb2xlLyQyIC0tcm9sZS1zZXNzaW9uLW5hbWUgJHJvbGVfc2Vzc2lvbl9uYW1lIC0tZHVyYXRpb24tc2Vjb25kcyAzNjAwIC0tb3V0cHV0IGpzb24pCmlmIFsgIiQ/IiAtbmUgMCBdOyB0aGVuIGV4aXQgMTsgZmkKZXhwb3J0IEFXU19BQ0NFU1NfS0VZX0lEPSQoZWNobyAiJHthd3NfY3JlZHN9IiB8IGdyZXAgQWNjZXNzS2V5SWQgfCBhd2sgLUYnIicgJ3twcmludCAkNH0nICkKZXhwb3J0IEFXU19TRUNSRVRfQUNDRVNTX0tFWT0kKGVjaG8gIiR7YXdzX2NyZWRzfSIgfCBncmVwIFNlY3JldEFjY2Vzc0tleSB8IGF3ayAtRiciJyAne3ByaW50ICQ0fScgKQpleHBvcnQgQVdTX1NFU1NJT05fVE9LRU49JChlY2hvICIke2F3c19jcmVkc30iIHwgZ3JlcCBTZXNzaW9uVG9rZW4gfCBhd2sgLUYnIicgJ3twcmludCAkNH0nICkKZXhwb3J0IEFXU19TRUNVUklUWV9UT0tFTj0kKGVjaG8gIiR7YXdzX2NyZWRzfSIgfCBncmVwIFNlc3Npb25Ub2tlbiB8IGF3ayAtRiciJyAne3ByaW50ICQ0fScgKQplY2hvICJzZXNzaW9uICckcm9sZV9zZXNzaW9uX25hbWUnIHZhbGlkIGZvciA2MCBtaW51dGVzIg== | base64 --decode > /tmp/assume_role.sh"
-  }
-
-  provisioner "local-exec" {
-    when = "create"
-    command = "/bin/bash -c '${self.triggers.cmd}'"
-  }
-
-  provisioner "local-exec" {
-    when = "destroy"
-    command = "/bin/bash -c '${self.triggers.cmd}'"
-  }
-}
-
 resource "null_resource" "cli_resource" {
   triggers = {
-    cmd_create = "${var.role == "0" ? "" : "${local.assume_role_cmd} && "}${var.cmd}"
-    cmd_destroy = "${var.role == "0" ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}"
+    assume_role = "echo cm9sZV9zZXNzaW9uX25hbWU9YGNhdCAvcHJvYy9zeXMva2VybmVsL3JhbmRvbS91dWlkIDI+L2Rldi9udWxsIHx8IGRhdGUgfCBja3N1bSB8IGN1dCAtZCAiICIgLWYgMWAKYXdzX2NyZWRzPSQoYXdzIHN0cyBhc3N1bWUtcm9sZSAtLXJvbGUtYXJuIGFybjphd3M6aWFtOjokMTpyb2xlLyQyIC0tcm9sZS1zZXNzaW9uLW5hbWUgJHJvbGVfc2Vzc2lvbl9uYW1lIC0tZHVyYXRpb24tc2Vjb25kcyAzNjAwIC0tb3V0cHV0IGpzb24pCmlmIFsgIiQ/IiAtbmUgMCBdOyB0aGVuIGV4aXQgMTsgZmkKZXhwb3J0IEFXU19BQ0NFU1NfS0VZX0lEPSQoZWNobyAiJHthd3NfY3JlZHN9IiB8IGdyZXAgQWNjZXNzS2V5SWQgfCBhd2sgLUYnIicgJ3twcmludCAkNH0nICkKZXhwb3J0IEFXU19TRUNSRVRfQUNDRVNTX0tFWT0kKGVjaG8gIiR7YXdzX2NyZWRzfSIgfCBncmVwIFNlY3JldEFjY2Vzc0tleSB8IGF3ayAtRiciJyAne3ByaW50ICQ0fScgKQpleHBvcnQgQVdTX1NFU1NJT05fVE9LRU49JChlY2hvICIke2F3c19jcmVkc30iIHwgZ3JlcCBTZXNzaW9uVG9rZW4gfCBhd2sgLUYnIicgJ3twcmludCAkNH0nICkKZXhwb3J0IEFXU19TRUNVUklUWV9UT0tFTj0kKGVjaG8gIiR7YXdzX2NyZWRzfSIgfCBncmVwIFNlc3Npb25Ub2tlbiB8IGF3ayAtRiciJyAne3ByaW50ICQ0fScgKQplY2hvICJzZXNzaW9uICckcm9sZV9zZXNzaW9uX25hbWUnIHZhbGlkIGZvciA2MCBtaW51dGVzIg== | base64 --decode > /tmp/assume_role.sh"
+    cmd_create = "${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.cmd}"
+    cmd_destroy = "${var.role == 0 ? "" : "${local.assume_role_cmd} && "}${var.destroy_cmd}"
   }
 
   provisioner "local-exec" {
     when    = "create"
-    command = "/bin/bash -c '${self.triggers.cmd_create}'"
+    command = "/bin/bash -c '${self.triggers.assume_role} && ${self.triggers.cmd_create}'"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "/bin/bash -c '${self.triggers.cmd_destroy}'"
+    command = "/bin/bash -c '${self.triggers.assume_role} && ${self.triggers.cmd_destroy}'"
   }
 
-  # By depending on the null_resource, the cli resource effectively depends on the existence
-  # of the resources identified by the ids provided via the dependency_ids list variable
-  # and the availability of assume role script
-  depends_on = [ "null_resource.dependencies", "null_resource.assume_role" ]
+  # By depending on the null_resource, the cli resource effectively depends on the existance
+  # of the resources identified by the ids provided via the dependency_ids list variable.
+  depends_on = [ "null_resource.dependencies" ]
 }
 
 resource "null_resource" "dependencies" {
-  triggers = {
+  triggers {
     dependencies = "${join(",", var.dependency_ids)}"
   }
 }
